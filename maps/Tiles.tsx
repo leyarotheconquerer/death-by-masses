76cfd47b7e829e5c4d1990b69ed2d296e9601978
leyarotheconquerer <leyarotheconquerer@yahoo.com>
2019-04-27 09:33:07 -0500

    Add basic tile set and test map

---
+++ maps/Tiles.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="2019.04.25" name="Tiles" tilewidth="256" tileheight="256" tilecount="16" columns="4">
+ <image source="Tiles.png" width="1024" height="1024"/>
+ <terraintypes>
+  <terrain name="Dirt" tile="3"/>
+  <terrain name="Grass" tile="5"/>
+ </terraintypes>
+ <tile id="0" type="Grass" terrain=",,,1"/>
+ <tile id="1" type="Grass" terrain=",,1,1"/>
+ <tile id="2" type="Grass" terrain=",,1,"/>
+ <tile id="3" type="Dirt" terrain="0,0,0,0"/>
+ <tile id="4" type="Grass" terrain=",1,,1"/>
+ <tile id="5" type="Grass" terrain="1,1,1,1"/>
+ <tile id="6" type="Grass" terrain="1,,1,"/>
+ <tile id="8" type="Grass" terrain=",1,,"/>
+ <tile id="9" type="Grass" terrain="1,1,,"/>
+ <tile id="10" type="Grass" terrain="1,,,"/>
+ <tile id="12" terrain="1,1,1,"/>
+ <tile id="13" terrain="1,1,,1"/>
+ <tile id="14" terrain="1,,1,1"/>
+ <tile id="15" terrain=",1,1,1"/>
+</tileset>
